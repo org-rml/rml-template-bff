c4e0e682884db98dfa726ce8e4cc9b7af38906ba
rodriigolima <rodrigomoreiralima@hotmail.com>
2026-06-09 21:59:41 -0300

    fix: adiciona TestPropertySource para server.parallelism no ProductControllerTest

--- src/test/java/br/com/rml/BFF_PROJECT/controller/ProductControllerTest.java
+++ src/test/java/br/com/rml/BFF_PROJECT/controller/ProductControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -31,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @WebMvcTest(ProductController.class)
 @Import(RestExceptionHandler.class)
+@TestPropertySource(properties = "server.parallelism=4")
 class ProductControllerTest {
 
     @Autowired
